ec758fd0e27aeb15cab606bc764ba49dd7572330
Emilis Voropajevas <emilisvoropajevas@gmail.com>
2026-05-21 14:11:33 +0100

    Added startup shell script for db

---
+++ backend/scripts/prestart.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# Let the DB start
+python app/backend_pre_start.py
+
+#Run migrations
+alembic upgrade head
+
+#Create initial data in db
+python app/initial_data.py
