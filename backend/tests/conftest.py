82a05d49bf620ab3ede3d39969a450053803fa1d
Emilis Voropajevas <emilisvoropajevas@gmail.com>
2026-05-22 11:34:00 +0100

    Added seed order creation pytest fixture with seed_order_data for testing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -9,18 +9,28 @@ from app.core.db import engine, init_db
 from app.main import app
 from app.models import Orders, Reports
 from tests.utils.utils import get_superuser_token_headers
+from tests.utils.seed_order_data import raw_test_data
 
 @pytest.fixture(scope="session", autouse=True)
 def db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         init_db(session)
         yield session
-        statement = delete(Orders)
-        session.exec(statement)
-        statement = delete(Reports)
-        session.exec(statement)
-        session.commit()
 
+@pytest.fixture(scope="session", autouse=True)
+def seed_order(db: Session) -> Generator[None, None, None]:
+    seed_data = [dict(zip(raw_test_data.keys(), values)) for values in zip(*raw_test_data.values())]
+    #Unpack dictionary of dict values for seed orders
+    orders = [Orders(**row) for row in seed_data]
+    db.add_all(orders)
+    db.commit()
+    yield
+    statement = delete(Orders)
+    db.exec(statement)
+    statement = delete(Reports)
+    db.exec(statement)
+    db.commit()
+    
 @pytest.fixture(scope="module")
 def client() -> Generator[TestClient, None, None]:
     with TestClient(app) as c:
@@ -29,7 +39,3 @@ def client() -> Generator[TestClient, None, None]:
 @pytest.fixture(scope="module")
 def superuser_token_headers(client: TestClient) -> dict[str,str]:
     return get_superuser_token_headers(client)
-
-#Seed data Orders and reports creation required
-
-#Upload endpoint fixture deletion required
---
+++ backend/tests/utils/seed_order_data.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+raw_test_data = {
+    "order_id": [
+        "442912","443547","441884","447418","441152",
+        "441127","442765","440424","443677","443069"
+    ],
+
+    "order_date": [
+        datetime(2026, 5, 10, 18, 59),
+        datetime(2026, 5, 4, 15, 5),
+        datetime(2026, 5, 10, 10, 51),
+        datetime(2026, 5, 10, 23, 0),
+        datetime(2026, 5, 15, 1, 12),
+        datetime(2026, 5, 15, 21, 44),
+        datetime(2026, 5, 11, 21, 57),
+        datetime(2026, 5, 20, 9, 40),
+        datetime(2026, 5, 20, 16, 19),
+        datetime(2026, 5, 15, 8, 21)
+    ],
+
+    "product_sku": [
+        "DWP/1935/01","DWP/1935/01","DWP/1935/01",
+        "DWP/1935/01","DWP/1935/01","DWP/1935/01",
+        "DWP/1935/01","DWP/1935/01","DWP/1935/01",
+        "DWP/1935/01"
+    ],
+
+    "product_name": [
+        "Product1","Product1","Product1","Product1","Product1",
+        "Product1","Product1","Product1","Product1","Product1"
+    ],
+
+    "price": [
+        10.1, 11.2, 10.1, 11.2, 10.1,
+        11.2, 10.1, 11.2, 10.1, 11.2
+    ],
+
+    "qty_ordered": [
+        43, 7, 56, 58, 105,
+        99, 46, 105, 21, 34
+    ],
+
+    "model_range": [
+        "DWP/1935","DWP/1935","DWP/1935","DWP/1935","DWP/1935",
+        "DWP/1935","DWP/1935","DWP/1935","DWP/1935","DWP/1935"
+    ]
+}
