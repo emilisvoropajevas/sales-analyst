9b792ae0ef032174b924070d41de14c4d619c31c
Emilis Voropajevas <emilisvoropajevas@gmail.com>
2026-05-27 14:06:40 +0100

    Fixed import error in db, included reports route in main

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, upload, orders
+from app.api.routes import login, upload, orders, reports
 from app.core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(upload.router)
 api_router.include_router(orders.router)
+api_router.include_router(reports.router)
 
 #frontend setup 
