e7498eec506cce81c49a5394d513c8f28bd3d757
Emilis Voropajevas <emilisvoropajevas@gmail.com>
2026-06-02 17:49:47 +0100

    Added bash script to generate client with openapi and move to frontend src folder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
+import {heyApiPlugin} from '@hey-api/vite-plugin'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), heyApiPlugin()],
 })
