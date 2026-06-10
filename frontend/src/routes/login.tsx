9a8cdd6749bd7c695d3bc9d3ef1a5adcf5c2bb2c
Emilis Voropajevas <emilisvoropajevas@gmail.com>
2026-06-10 14:24:08 +0100

    Designed first login section without function for the moment :(

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { useState } from "react";
 import { toast } from 'sonner'
+
+import catImage from "../assets/overlooking-cat.png"
 import useAuth, {isLoggedIn} from "../hooks/useAuth";
 
 export const Route = createFileRoute("/login")({
@@ -35,11 +37,39 @@ function Login() {
         if (loginMutation.isPending) return
         loginMutation.mutate({ username, password })        
     }
+// Add cat image above h2 tag later
+    return (
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-black"> Sign in</h2>
 
+            <div className="mt-10">
+                <form className="space-y-6">
+                    <div>
+                        <label className="block text-sm/6 font-medium text-gray-700">Username</label>
+                        <div className="mt-2">
+                            <input id="username" type="text" name="username" required autoComplete="username" className="block w-full rounded-md bg-white/5 px-3 text-base text-black" />
+                        </div>
+                    </div>
 
+                    <div className="flex items-center justify-between">
+                        <label className="block font-medium text-gray-700">Password</label>
 
+                        <div className="mt-2">
+                            <input id="password" type="text" name="password" required autoComplete="username" className="block w-full rounded-md bg-white/5 px-3 text-base text-black" />
+                        </div>
+                    </div>
+                    <div>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5">Log In</button>
+                    </div>
 
+                </form>
 
+            </div>
+        </div>
+        
+
+    )
 }
 
 // Full Page centered container
